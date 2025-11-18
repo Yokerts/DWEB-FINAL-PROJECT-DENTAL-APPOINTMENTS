2edaff476a228a8962dc1341ab128dc7ddabdaa3
acrescodev <acresco.dev@gmail.com>
2025-11-18 09:44:32 -0600

    change wsgi.py

--- app/config/wsgi.py
+++ app/config/wsgi.py
@@ -1,6 +1,10 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.config.settings')
+# Agregar carpeta 'app' al path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
