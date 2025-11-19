9d12c10021351c7c3d9fb1efb4bc34f1cf3b9f1c
acrescodev <acresco.dev@gmail.com>
2025-11-19 06:17:22 -0600

    Add superuser

--- app/create_superuser.py
+++ app/create_superuser.py
@@ -1,14 +1,20 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+django.setup()
+
 from django.contrib.auth import get_user_model
-from django.conf import settings
 
 User = get_user_model()
 
-username = "admin"
-email = "ing_medina16@hotmail.com"
-password = "yktsadmin1234"
+username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL", "ing_medina16@hotmail.com")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "yktsadmin1234")
 
 if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superusuario creado.")
+    User.objects.create_superuser(username, email, password)
+    print("Superusuario creado automáticamente.")
 else:
     print("El superusuario ya existe.")
